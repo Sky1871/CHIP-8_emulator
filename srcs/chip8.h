9a8a7072cd153e09fadee25dc14dc61595c8dba8
ENBY <seltenreichkristof@gmail.com>
2026-08-16 19:24:39 +0200

    uint8_t

--- srcs/chip8.h
+++ srcs/chip8.h
@@ -1,23 +1,21 @@
-#ifndef CHIP8_H
-#define CHIP8_H
+#pragma once
 
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct CHIP8 {
-  unsigned short opcode; // store current opcode //
+typedef struct {
+  uint8_t opcode; // store current opcode //
   unsigned char memory[4096];
   unsigned char
       V[16]; // CPU registers: 15 general purpose (V0-VE), 16th is carry flag //
 
-  unsigned short I; // Index register (0x000-0xFFF) //
-  unsigned short
-      pc; // program counter (0x000-0xFFF) //
-          // 0x000-0x1FF - Chip 8 interpreter (contains font set in emu) //
-          // 0x050-0x0A0 - Used for the built in 4x5 pixel font set (0-F) //
-          // 0x200-0xFFF - Program ROM and work RAM //
+  uint8_t I;  // Index register (0x000-0xFFF) //
+  uint8_t pc; // program counter (0x000-0xFFF) //
+              // 0x000-0x1FF - Chip 8 interpreter (contains font set in emu) //
+              // 0x050-0x0A0 - Used for the built in 4x5 pixel font set (0-F) //
+              // 0x200-0xFFF - Program ROM and work RAM //
 
   unsigned char gfx[64 * 32]; // 2048px //
 
@@ -26,11 +24,9 @@ typedef struct CHIP8 {
   unsigned char delay_timer;
   unsigned char sound_timer;
 
-  unsigned short stack[16]; // stack is used to remember the current location
-                            // before a jump is performed //
-  unsigned short sp;        // stack pointer //
+  uint8_t stack[16]; // stack is used to remember the current location
+                     // before a jump is performed //
+  uint8_t sp;        // stack pointer //
 
   unsigned char key[16]; // HEX based keypad (0x0-0xF) //
 } chip8;
-
-#endif
